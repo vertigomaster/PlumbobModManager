d7c9b2c20997b9257fe9ceaa80bc519d14ecc880
vertigomaster <janoel777@gmail.com>
2026-04-02 22:30:54 -0500

    started playing with CommandLine

--- TS4Plumbob.CLI/Program.cs
+++ TS4Plumbob.CLI/Program.cs
@@ -1,15 +1,57 @@
-﻿using TS4PlumbobCore.DataModels;
+﻿using TS4Plumbob.Core.DataModels;
+using System.Text.Json.Serialization;
+using System.CommandLine;
 
-namespace PlumbobMMCLI;
+namespace Plumbob.CLI;
 
 class Program
 {
-    static void Main(string[] args)
+    string AppFolder => AppContext.BaseDirectory;
+    string AppConfigDir => Path.Combine(AppFolder, "config");
+    
+    private static bool persistentMode = false;
+    static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Hello, World! It's Plumbob Mod Manager time! Woo!");
-
-        AuthorProfile profile = new AuthorProfile("Author Mr. Vertigo", new []{"bob.com"}, "https://github.com/vertigomaster");
+        Console.Title = "Plumbob Mod Manager";
         
-        Console.WriteLine($"Profile Name: {profile.Name}, at {profile.MainModSiteUrl}");
+        //if no args, prompt for a line, break it into tokens, and run that?
+        
+        RootCommand rootCommand = new("Plumbob Mod Manager CLI (Command Line Interface) for The Sims 4. Directly runs PMM commands.");
+
+        Command testCommand = new("test", "testing out subcommands")
+        {
+            new Option<bool>("--example"),
+            new Option<bool>("--fart-mode")
+        };
+        rootCommand.Subcommands.Add(testCommand);
+
+        
+        
+        Command rigCommand = new("rig", "Commands associated with whole mod rigs.");
+        rootCommand.Subcommands.Add(rigCommand);
+        
+        Command createRigCommand = new("create", "Creates a rig");
+        Command selectRigCommand = new("select", "selects a rig");
+        Command deleteRigCommand = new("delete", "Deletes a rig");
+        
+        rigCommand.Subcommands.Add(createRigCommand);
+        rigCommand.Subcommands.Add(selectRigCommand);
+        rigCommand.Subcommands.Add(deleteRigCommand);
+        
+        Command addModArchiveCommand = new("add-mod", "adds a new mod archive");
+        rootCommand.Subcommands.Add(addModArchiveCommand);
+
+        var parseResult = rootCommand.Parse(args);
+        return await parseResult.InvokeAsync();
+    }
+
+    static void SaveConfigFile()
+    {
+        
+    }
+
+    static void LoadConfigFile()
+    {
+        
     }
 }
