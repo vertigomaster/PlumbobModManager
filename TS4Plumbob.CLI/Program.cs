6926fa3e504e935efca6dcd26cf56fdcd9df1cd6
vertigomaster <janoel777@gmail.com>
2026-04-05 13:14:33 -0500

    structural updates on CLI commands (no commands executing yet though)

--- TS4Plumbob.CLI/Program.cs
+++ TS4Plumbob.CLI/Program.cs
@@ -6,12 +6,8 @@ namespace Plumbob.CLI;
 
 class Program
 {
-    string AppFolder => AppContext.BaseDirectory;
-    string AppConfigDir => Path.Combine(AppFolder, "config");
-    
-    
-    
     private static bool persistentMode = false;
+    
     static async Task<int> Main(string[] args)
     {
         Console.Title = "Plumbob Mod Manager";
@@ -20,31 +16,51 @@ class Program
         
         RootCommand rootCommand = new("Plumbob Mod Manager CLI (Command Line Interface) for The Sims 4. Directly runs PMM commands.");
 
+        var testCommand = BuildTestCommands();
+        rootCommand.Subcommands.Add(testCommand);
+        
+        var rigCommand = BuildRigCommands();
+        rootCommand.Subcommands.Add(rigCommand);
+
+        var modMetaCommand = BuildModCommands();
+        rootCommand.Subcommands.Add(modMetaCommand);
+
+        var parseResult = rootCommand.Parse(args);
+        return await parseResult.InvokeAsync();
+    }
+
+    private static Command BuildTestCommands()
+    {
         Command testCommand = new("test", "testing out subcommands")
         {
             new Option<bool>("--example"),
             new Option<bool>("--fart-mode")
         };
-        rootCommand.Subcommands.Add(testCommand);
+        return testCommand;
+    }
 
-        
-        
-        Command rigCommand = new("rig", "Commands associated with whole mod rigs.");
-        rootCommand.Subcommands.Add(rigCommand);
+    private static Command BuildRigCommands()
+    {
+        Command rigMetaCommand = new("rig", "Commands associated with whole mod rigs.");
         
         Command createRigCommand = new("create", "Creates a rig");
         Command selectRigCommand = new("select", "selects a rig");
         Command deleteRigCommand = new("delete", "Deletes a rig");
         
-        rigCommand.Subcommands.Add(createRigCommand);
-        rigCommand.Subcommands.Add(selectRigCommand);
-        rigCommand.Subcommands.Add(deleteRigCommand);
-        
-        Command addModArchiveCommand = new("add-mod", "adds a new mod archive");
-        rootCommand.Subcommands.Add(addModArchiveCommand);
+        rigMetaCommand.Subcommands.Add(createRigCommand);
+        rigMetaCommand.Subcommands.Add(selectRigCommand);
+        rigMetaCommand.Subcommands.Add(deleteRigCommand);
+        return rigMetaCommand;
+    }
 
-        var parseResult = rootCommand.Parse(args);
-        return await parseResult.InvokeAsync();
+    private static Command BuildModCommands()
+    {
+        Command modMetaCommand = new("mod", "mod meta-command");
+
+        Command addModArchiveCommand = new("add-mod", "adds a new mod archive");
+
+        modMetaCommand.Subcommands.Add(addModArchiveCommand);
+        return modMetaCommand;
     }
 
     static void SaveConfigFile()
