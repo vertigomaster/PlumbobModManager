52546c0ee77922b47ea67e91c25ac777ed60bc1b
vertigomaster <janoel777@gmail.com>
2026-04-14 21:35:37 -0500

    Added json test for understanding ReferenceHandler.Preserve

---
+++ PlumbobModManager.Tests/JsonPreserveRepro.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using NUnit.Framework;
+
+namespace PlumbobModManager.Tests;
+
+public record SimpleModEntry(string Name);
+
+public class SimpleLib
+{
+    [JsonInclude]
+    public List<SimpleModEntry> Mods = new();
+}
+
+public class JsonPreserveReproTests
+{
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        WriteIndented = true,
+        ReferenceHandler = ReferenceHandler.Preserve
+    };
+
+    [Test]
+    public void Repro_ReferenceNotFound()
+    {
+        var lib = new SimpleLib();
+        lib.Mods.Add(new SimpleModEntry("Test"));
+
+        string json = JsonSerializer.Serialize(lib, Options);
+        Console.WriteLine("[DEBUG_LOG] JSON: " + json);
+
+        var deserialized = JsonSerializer.Deserialize<SimpleLib>(json, Options);
+        Assert.That(deserialized.Mods.Count, Is.EqualTo(1));
+    }
+}
