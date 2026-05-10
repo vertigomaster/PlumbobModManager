0d9b6bc264f2fb6a74edfd75de02d83fda2ea066
vertigomaster <janoel777@gmail.com>
2026-05-10 03:46:03 -0500

    Started adding new mod slug tests...which are immediately failing! So that's fun.
    These edge cases are not a huge priority and merging them to master before resolving them slows everything else down (as not all tests will pass)

---
+++ PlumbobModManager.Tests/ModSlug.Test.cs
@@ -0,0 +1,29 @@
+﻿using TS4Plumbob.Core.DataModels;
+
+namespace PlumbobModManager.Tests;
+
+public class ModSlug_Test : AbstractPlumbobTest
+{
+    [Test]
+    public void SanitizeForSlug_BasicSanitation()
+    {
+        string input = "This is a test string with invalid characters";
+        string expectedOutput = "this_is_a_test_string_with_invalid_characters";
+        string actualOutput = ModSlug.SanitizeForSlug(input);
+
+        Assert.That(actualOutput, Is.EqualTo(expectedOutput),
+            $"Spaces should be removed during slugification and everything should become lowercase!\n" +
+            $" got: '{actualOutput}' but expected: '{expectedOutput}'");
+    }
+    [Test]
+    public void SanitizeForSlug_RemovalOfRiskySymbolChars()
+    {
+        string input = "This is a test string with invalid characters: !@#$%^&*()";
+        string expectedOutput = "this_is_a_test_string_with_invalid_characters";
+        string actualOutput = ModSlug.SanitizeForSlug(input);
+
+        Assert.That(actualOutput, Is.EqualTo(expectedOutput),
+            $"Invalid or risky symbol characters should be removed during slugification\n" +
+            $" got: '{actualOutput}' but expected: '{expectedOutput}'");
+    }
+}
